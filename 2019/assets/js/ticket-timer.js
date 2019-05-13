a1c61f7238e6a1b174d91e4b2e8ceb88526377b0
Jane Herriman <xorJane@users.noreply.github.com>
2019-05-13 11:24:11 -0700

    Try again to fix ticket sales link

--- 2019/assets/js/ticket-timer.js
+++ 2019/assets/js/ticket-timer.js
@@ -40,7 +40,7 @@ function countdown(date, id){
 
 if( (earlyBird - new Date().getTime()) > 0 ){
 
-    $("#ticket-timer-text").html("<a href="https://juliacon.org/2019/tickets/">Early Bird Ticket Sale </a>");
+    $("#ticket-timer-text").html("<a href='https://juliacon.org/2019/tickets/'>Early Bird Ticket Sale </a>");
 
     // Update countdown every 1 second
     var cd1 = setInterval(
